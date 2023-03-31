cc74aefe8794616d8f8d494e9584c31ba6505adc
RullKhrl <khoirulmustofa767@gmail.com>
2023-03-31 10:02:36 +0700

    Update daun-berguguran.js

--- daun-berguguran.js
+++ daun-berguguran.js
@@ -41,7 +41,7 @@
 			  am = new Array();
 			  stx = new Array();
 			  sty = new Array();
-			  snowsrc=(snowsrc.indexOf("dynamicdrive.com")!=-1)? "snow.gif" : snowsrc
+			  snowsrc=(snowsrc.indexOf("dynamicdrive.com")!=-1)? "gambar-transparent-leaf-vector-png_42880.png" : snowsrc
 			  for (i = 0; i < no; ++ i) {  
 				dx[i] = 0;                        // set coordinate variables
 				xp[i] = Math.random()*(doc_width-50);  // set position variables
